1370e4b89029095656436a276dea13e94ead2760
Ezequiel Arangue <eze.arangue@gmail.com>
2020-08-03 13:08:14 -0300

    System properties

--- src/test/java/com/example/unitconverter/UnitConverterApplicationTests.java
+++ src/test/java/com/example/unitconverter/UnitConverterApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example.unitconverter;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.example.unitconverter.controller.TemperatureController;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class UnitConverterApplicationTests {
 
+	@Autowired
+	private TemperatureController controller;
+
 	@Test
 	void contextLoads() {
+		assertThat(controller).isNotNull();
 	}
 
 }
